129a91c2790fefa8706e04281b58165c5b633109
NicolasJott <nicobox2002@gmail.com>
2023-02-22 15:16:15 -0500

    added website link to workout project

--- features/projects.js
+++ features/projects.js
@@ -4,6 +4,7 @@ const projects = [
         name: "WorkoutTracker",
         description: "An easy to use app for logging your workouts",
         img: "images/workoutTracker.png",
+        url: "http://3.19.209.225",
         github: "https://github.com/NicolasJott/workoutTracker",
         tech: ["React.js", "Express", "MongoDB", "Node.js"],
     },
@@ -34,17 +35,31 @@ window.addEventListener('DOMContentLoaded', function() {
         const ul = this.document.createElement("ul");
         let innerDiv = document.createElement('div');
         let topDiv = document.createElement('div');
-        let i = document.createElement('i');
-        let a = document.createElement('a');
+        let hrefDiv = this.document.createElement('div');
+        let git = document.createElement('i');
+        let gitA = document.createElement('a');
         let img = document.createElement('img');
         img.setAttribute("src", project.img)
         img.setAttribute("alt", `${project.name} image`)
         
-        i.classList.add('fa-brands');
-        i.classList.add('fa-github')
-        a.setAttribute("href", project.github)
-        a.classList.add('github')
-        a.appendChild(i)
+        if (project.url) {
+            let url = this.document.createElement('i')
+            let urlA = this.document.createElement('a')
+            url.classList.add('fa-solid');
+            url.classList.add('fa-globe');
+            urlA.setAttribute("href", project.url)
+            urlA.classList.add('github')
+            urlA.appendChild(url)
+            hrefDiv.appendChild(urlA)
+
+
+        }
+        git.classList.add('fa-brands');
+        git.classList.add('fa-github')
+        gitA.setAttribute("href", project.github)
+        gitA.classList.add('github')
+        gitA.appendChild(git)
+        hrefDiv.appendChild(gitA);
 
         h3.textContent = project.name;
         p.textContent = project.description
@@ -57,7 +72,9 @@ window.addEventListener('DOMContentLoaded', function() {
         span.appendChild(ul);
     
         topDiv.appendChild(h3);
-        topDiv.appendChild(a);
+        topDiv.appendChild(hrefDiv)
+        
+        
 
         topDiv.classList.add('top-div')
 
